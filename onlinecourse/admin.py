c32082dcd6e954af43f571c7c56a27573dac5e6a
ramineb <ramin.ebrahimi75@gmail.com>
2026-05-08 13:38:52 -0400

    new update file admin in online course

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+    
 class ChoiceInline(admin.StackedInline):
 		model = Choice
 		extra = 2
@@ -9,10 +13,6 @@ class ChoiceInline(admin.StackedInline):
 		model = Question
 		extra = 2
 
-
-class LessonInline(admin.StackedInline):
-    model = Lesson
-    extra = 5
 
 
 # Register your models here.
@@ -23,13 +23,12 @@ class CourseAdmin(admin.ModelAdmin):
     search_fields = ['name', 'description']
 
 
-class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
-
-
 class QuestionAdmin(admin.ModelAdmin):
 		inlines = [ChoiceInline]
 		list_display = ['content']
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
